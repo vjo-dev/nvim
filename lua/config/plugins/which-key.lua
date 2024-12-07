a514e12d74007c2b3a825c0309c7b0b3405a30cd
Vincent Journot <journot.vincent@gmail.com>
2024-12-07 20:46:20 +0100

    fix which-key issues, new declaration

--- lua/config/plugins/which-key.lua
+++ lua/config/plugins/which-key.lua
@@ -15,24 +15,28 @@
 return {
 	{ -- Useful plugin to show you pending keybinds.
 		"folke/which-key.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		event = "VimEnter", -- Sets the loading event to 'VimEnter'
 		config = function() -- This is the function that runs, AFTER loading
 			require("which-key").setup()
 
-			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>f"] = { name = "[F]find", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-			})
+			local wk = require("which-key")
 
-			-- visual mode
-			require("which-key").register({
-				["<leader>h"] = { "Git [H]unk" },
-			}, { mode = "v" })
+			wk.add({
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>c_", hidden = true },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>d_", hidden = true },
+				{ "<leader>f", group = "[F]find" },
+				{ "<leader>f_", hidden = true },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>r_", hidden = true },
+				{ "<leader>t", group = "[T]oggle" },
+				{ "<leader>t_", hidden = true },
+				{ "<leader>w", group = "[W]orkspace" },
+				{ "<leader>w_", hidden = true },
+				{ "<leader>h", desc = "Git [H]unk", mode = "v" },
+			})
 		end,
 	},
 }

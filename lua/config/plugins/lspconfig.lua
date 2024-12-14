0f41382de67502d61aa2e3349cf7b1e131612eab
Vincent Journot <journot.vincent@gmail.com>
2024-12-14 08:51:10 +0100

    fix bug with tssserver refactor as ts_ls

--- lua/config/plugins/lspconfig.lua
+++ lua/config/plugins/lspconfig.lua
@@ -173,7 +173,8 @@ return {
 			--    https://github.com/pmizio/typescript-tools.nvim
 			--
 			-- But for many setups, the LSP (`tsserver`) will work just fine
-			tsserver = {},
+			-- tsserver = {},
+      ts_ls = {},
 			--
 
 			lua_ls = {

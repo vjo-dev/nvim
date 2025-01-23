6093a3bf24e252c061bc4a21bd481e6b58f4b12c
Vincent Journot <journot.vincent@gmail.com>
2025-01-23 21:09:03 +0100

    add ts-tool plugin

---
+++ lua/config/plugins/ts-tools.lua
@@ -0,0 +1,11 @@
+-- https://github.com/pmizio/typescript-tools.nvim
+
+return {}
+-- return {
+-- 	"pmizio/typescript-tools.nvim",
+-- 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+-- 	opts = {},
+-- 	config = function()
+-- 		require("typescript-tools").setup({})
+-- 	end,
+-- }

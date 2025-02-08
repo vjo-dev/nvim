750d5a7b83fee3f37d7a2d4d30a06fc4f401900f
Vincent Journot <journot.vincent@gmail.com>
2025-02-08 14:40:22 +0100

    set transparent background

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,10 +1,15 @@
 -- https://github.com/catppuccin/nvim
 
 return {
-  "catppuccin/nvim",
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    -- Load the colorscheme here.
-    vim.cmd.colorscheme "catppuccin-mocha"
-  end,
+	"catppuccin/nvim",
+	priority = 1000, -- Make sure to load this before all the other start plugins.
+	init = function()
+		-- Load the colorscheme here.
+		vim.cmd.colorscheme("catppuccin-mocha")
+	end,
+	config = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+		})
+	end,
 }
